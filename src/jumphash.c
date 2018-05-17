4c9b2630468f915cafcff7d01d4be5ef45f5ffff
Eric Herman <eric@freesa.org>
2018-05-17 10:15:07 +0200

    LGPLv2.1+

--- src/jumphash.c
+++ src/jumphash.c
@@ -4,7 +4,7 @@ Copyright (C) 2018 Eric Herman <eric@freesa.org>
 
 This work is free software: you can redistribute it and/or modify it
 under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at
+the Free Software Foundation, either version 2.1 of the License, or (at
 your option) any later version.
 
 This work is distributed in the hope that it will be useful, but WITHOUT
@@ -12,8 +12,7 @@ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
-        https://www.gnu.org/licenses/lgpl-3.0.txt
-        https://www.gnu.org/licenses/gpl-3.0.txt
+	https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
 
 Much of this code was from 2014 by John Lamping, Eric Veach of Google
 https://arxiv.org/pdf/1406.2294v1.pdf
